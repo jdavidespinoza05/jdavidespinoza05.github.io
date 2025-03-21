ab37b58a50dcdb2f6fdf428a6339dc9896680d89
jdavidespinoza05 <jdavidespinoza05@gmail.com>
2025-03-20 22:08:42 -0600

    imagenes con kit

--- docs/index.js
+++ docs/index.js
@@ -1,16 +1,6 @@
 const learnMoreBtn = document.getElementById("learnMoreBtn");
 const moreLinks = document.getElementById("moreLinks");
 
-learnMoreBtn.addEventListener("click", function() {
-    if (moreLinks.classList.contains("show")) {
-        moreLinks.classList.remove("show"); // Ocultar con animación
-        setTimeout(() => {
-            moreLinks.style.display = "none"; // Ocultar completamente después de la animación
-        }, 500); // Tiempo igual al de la transición en CSS
-    } else {
-        moreLinks.style.display = "block"; // Mostrar antes de la animación
-        setTimeout(() => {
-            moreLinks.classList.add("show"); // Aplicar animación
-        }, 10); // Pequeño retraso para que el display block funcione
-    }
+learnMoreBtn.addEventListener("click", function () {
+    moreLinks.classList.toggle("show"); // Alternar la clase sin modificar "display"
 });
